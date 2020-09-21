4e300841f1217854967ee26beffb1d404960e838
akm0012 <akm0012@auburn.edu>
2020-09-20 22:56:33 -0400

    Code cleanup

---
+++ app/src/test/java/com/andrewkingmarshall/videogamelibrary/viewmodel/MainActivityViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.andrewkingmarshall.videogamelibrary.viewmodel
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainActivityViewModelTest {
+
+    // Todo: Test business logic
+}
